55a5aca2d58d4165d724711355fb931413b82e9c
yunhozz <qkrdbsgh1121@naver.com>
2022-11-16 10:33:50 +0900

    Initial commit

---
+++ src/main/java/com/openapitest/OpenapiTestApplication.java
@@ -0,0 +1,13 @@
+package com.openapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openapitest/OpenapiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
